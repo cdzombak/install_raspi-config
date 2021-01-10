7b41aa5f7d9e429df7eed5060f90d533ae9b8f99
Chris Dzombak <chris@dzombak.com>
2021-01-09 21:22:22 -0500

    modify script to my own preferences

--- install.sh
+++ install.sh
@@ -1,20 +1,29 @@
-#!/bin/sh
-# Check if root
+#!/usr/bin/env bash
+set -eu
+
+# grab the latest .deb filename from https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/
+# and copy it here to update this script:
+RASPI_CONFIG_DEB="raspi-config_20201108_all.deb"
+
 if [ "$(whoami)" != "root" ]; then
-  whiptail --msgbox "Sorry you are not root. You must type: sudo sh install.sh" $WT_HEIGHT $WT_WIDTH
-  exit
+  echo "Sorry, you are not root. You must type: sudo ./install.sh"
+  exit 1
 fi
 
-# Check if raspi-config is installed
+if ! command -v apt-get ; then
+  echo "This install script only works on Debian-derived distributions."
+  exit 2
+fi
+
 if [ $(dpkg-query -W -f='${Status}' raspi-config 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-  whiptail --msgbox "Raspi-config is already installed, try upgrading it within raspi-config..." 10 60
-else
-  wget https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20200601_all.deb -P /tmp
-  apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils -y
-  # Auto install dependancies on eg. ubuntu server on RPI
-  apt-get install -fy
-  dpkg -i /tmp/raspi-config_20200601_all.deb
-  whiptail --msgbox "Raspi-config is now installed, run it by typing: sudo raspi-config" 10 60
+  echo "raspi-config is already installed. Try upgrading it within raspi-config."
+  exit 3
 fi
 
-exit
+apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils -y
+apt-get install -fy
+
+wget -O /tmp/raspi-config.deb https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/$RASPI_CONFIG_DEB
+dpkg -i /tmp/raspi-config.deb
+
+echo "raspi-config is now installed. Run it by typing: sudo raspi-config"

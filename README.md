313b132f7af94adfe045b96b21f386cd88d886a0
Chris Dzombak <chris@dzombak.com>
2021-01-11 11:09:45 -0500

    automatically get the latest version

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
-install_raspi-config
-====================
+# install_raspi-config
 
-Script to install raspi-config on Raspberry Pi devices with a Linux OS that's missing it, like Kali Linux.
+Script to install the latest `raspi-config` on Raspberry Pi devices with a Linux OS that's missing it, like Kali Linux.
 
-Must be run as root or with sudo:
-
-```
-sudo ./install.sh
-```
-
-The reference for the raspi-config package being installed might not be the latest one, since I don't update this script with each new version. So use the installed raspi-config to update itself, or modify [line 6](https://github.com/cdzombak/install_raspi-config/blob/master/install.sh#L6) before installing.
+Must be run as `root` or with `sudo`: `sudo ./install.sh`
--- install.sh
+++ install.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 set -eu
 
-# grab the latest .deb filename from https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/
-# and copy it here to update this script:
-RASPI_CONFIG_DEB="raspi-config_20201108_all.deb"
-
 if [ "$(whoami)" != "root" ]; then
-  echo "Sorry, you are not root. You must type: sudo ./install.sh"
+  echo "This script must be run as root: sudo ./install.sh"
   exit 1
 fi
 
 if ! command -v apt-get ; then
-  echo "This install script only works on Debian-derived distributions."
-  exit 2
-fi
-
-if [ $(dpkg-query -W -f='${Status}' raspi-config 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-  echo "raspi-config is already installed. Try upgrading it within raspi-config."
-  exit 3
+  echo "This script only works on Debian-derived distributions."
+  exit 1
 fi
 
 apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils -y
 apt-get install -fy
 
-wget -O /tmp/raspi-config.deb https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/$RASPI_CONFIG_DEB
-dpkg -i /tmp/raspi-config.deb
+RASPI_CONFIG_DEB="$(curl -s https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/ | grep -E -o "raspi-config_\d\d\d\d\d\d\d\d_all.deb" | tail -n 1)"
 
-echo "raspi-config is now installed. Run it by typing: sudo raspi-config"
+echo ""
+echo "Downloading & installing $RASPI_CONFIG_DEB ..."
+echo ""
+
+wget -O /tmp/raspi-config.deb "https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/$RASPI_CONFIG_DEB"
+dpkg -i /tmp/raspi-config.deb
+rm /tmp/raspi-config.deb
+
+echo "raspi-config is now installed."
+echo "Run it by typing: sudo raspi-config"
